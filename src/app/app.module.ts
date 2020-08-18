6d412b6dd1464c4b8827b4d9aa3e2c72c69f1264
Vladoxx <wladox@outlook.com>
2020-08-18 05:27:38 -0300

    Diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { NgclassComponent } from './ngclass/ngclass.component';
+import { NgcontentComponent } from './ngcontent/ngcontent.component';
+import { NgstyleComponent } from './ngstyle/ngstyle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent,
+    NgclassComponent,
+    NgcontentComponent,
+    NgstyleComponent
   ],
   imports: [
     BrowserModule
